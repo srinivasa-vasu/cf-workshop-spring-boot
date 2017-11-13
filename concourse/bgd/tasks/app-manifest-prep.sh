ce2cddac0fbdc8ca0b626029d528b0c76536b68b
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2017-11-13 09:48:50 +0800

    path update

--- concourse/bgd/tasks/app-manifest-prep.sh
+++ concourse/bgd/tasks/app-manifest-prep.sh
@@ -4,6 +4,8 @@ set -xe
 
 cat ./app-info/current-app.txt
 
-sed "s/boot-app/$(cat ./app-info/next-app.txt)-$APP_SUFFIX/" ./manifest.yml > ./app-manifest-output/manifest.yml
+cd git-repo
 
-cat ./app-manifest-output/manifest.yml
+sed "s/boot-app/$(cat ./app-info/next-app.txt)-$APP_SUFFIX/" ./manifest.yml > ../app-manifest-output/manifest.yml
+
+cat ../app-manifest-output/manifest.yml
