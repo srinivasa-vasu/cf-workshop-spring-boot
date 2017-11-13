7b4ec54ab30d1716a687b8f261145c6574becffa
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2017-11-13 14:46:42 +0800

    route file update

--- concourse/bgd/tasks/update-routes.sh
+++ concourse/bgd/tasks/update-routes.sh
@@ -13,9 +13,10 @@ cf routes
 export DOMAIN_NAME=$APP_DOMAIN
 export ROUTE_HOSTNAME=$APP_SUFFIX
 
-export APP_NAME=$(cat ./app-info/next-app.txt)-$APP_SUFFIX
+# flip the app names as the previous task had already deployed the new one
+export APP_NAME=$(cat ./app-info/current-app.txt)-$APP_SUFFIX
 
-export CURRENT_APP_NAME=$(cat ./app-info/current-app.txt)-$APP_SUFFIX
+export CURRENT_APP_NAME=$(cat ./app-info/next-app.txt)-$APP_SUFFIX
 
 echo "Mapping main app route to point to $APP_NAME instance"
 cf map-route $APP_NAME $DOMAIN_NAME -n $ROUTE_HOSTNAME
