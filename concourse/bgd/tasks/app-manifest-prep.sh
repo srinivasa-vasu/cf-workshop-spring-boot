8f09c709f4262bf79fb18750271ecc71878e9ee8
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2017-11-13 13:55:57 +0800

    pipeline update

--- concourse/bgd/tasks/app-manifest-prep.sh
+++ concourse/bgd/tasks/app-manifest-prep.sh
@@ -6,6 +6,6 @@ cat ./app-info/current-app.txt
 
 cd git-repo
 
-sed "s/boot-app/$(cat ./app-info/next-app.txt)-$APP_SUFFIX/" ./manifest.yml > ../app-manifest-output/manifest.yml
+sed "s/$APP_SUFFIX/$(cat ../app-info/next-app.txt)-$APP_SUFFIX/" ./manifest.yml > ../app-manifest-output/manifest.yml
 
 cat ../app-manifest-output/manifest.yml
