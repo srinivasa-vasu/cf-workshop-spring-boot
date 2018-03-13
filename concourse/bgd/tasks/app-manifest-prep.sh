3a441019f5c1f870f6f7a0701a7c3cd0ab9dd840
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2018-03-13 15:31:38 +0530

    add target

--- concourse/bgd/tasks/app-manifest-prep.sh
+++ concourse/bgd/tasks/app-manifest-prep.sh
@@ -8,4 +8,4 @@ cd git-repo
 
 sed "s/$APP_SUFFIX/$(cat ../app-info/next-app.txt)-$APP_SUFFIX/" ./manifest.yml > ../app-manifest-output/manifest.yml
 
-cat ../app-manifest-output/manifest.yml
+cat ../app-manifest-output/manifest.yml 
