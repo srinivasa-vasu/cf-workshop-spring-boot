eed01654329c4bc755bb74f620d7b676db48e2ff
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2019-02-11 10:41:12 +0530

    update dep

--- concourse/shared/tasks/build-package.sh
+++ concourse/shared/tasks/build-package.sh
@@ -7,4 +7,7 @@ version=`cat $version`
 
 cd git-repo
 mvn package
+
+#mvn clean verify deploy -Ddistribution.management.release.id=artifactory-local -Ddistribution.management.release.url=http://jfrog.humourmind.com:8081/artifactory/libs-release-local
+
 cp target/cf-workshop-spring-boot-*.jar ../artifacts/boot-app-$version.jar
